7c50bd67b34e2c51fac284f7aaa4fcefafb721d1
maksym <1cknnkc1@gmail.com>
2025-09-22 14:27:16 +0200

    fixed image bug

--- src/main/java/com/example/pro2_kovalov_bazar/controllers/ImageController.java
+++ src/main/java/com/example/pro2_kovalov_bazar/controllers/ImageController.java
@@ -4,6 +4,7 @@ import com.example.pro2_kovalov_bazar.models.Image;
 import com.example.pro2_kovalov_bazar.repositories.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +22,14 @@ public class ImageController {
     private final ImageRepository imageRepository;
 
     @GetMapping("/images/{id}")
-    private ResponseEntity<?> getImageById(@PathVariable Long id) {
-        Image image = imageRepository.findById(id).orElse(null);
+    private ResponseEntity<InputStreamResource> getImageById(@PathVariable Long id) throws UnsupportedEncodingException {
+        Image image = imageRepository.findById(id).orElseThrow(() -> new RuntimeException("Image not found"));
+
+        String encodedFileName = URLEncoder.encode(image.getOriginalFileName(), StandardCharsets.UTF_8);
+
         return ResponseEntity.ok()
-                .header("fileName", image.getOriginalFileName())
-                .contentType(MediaType.valueOf(image.getContentType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename*=UTF-8''" + encodedFileName)
+                .contentType(MediaType.parseMediaType(image.getContentType()))
                 .contentLength(image.getSize())
                 .body(new InputStreamResource(new ByteArrayInputStream(image.getBytes())));
     }
